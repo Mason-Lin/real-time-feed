f3fae0f2d9235eb53ea9ded7bdf571a8605c3e3e
Mason Lin <pizza0117@gmail.com>
2021-04-24 17:02:44 +0800

    unit test draft

--- test_feed_parser.py
+++ test_feed_parser.py
@@ -56,6 +56,86 @@ class TestParser(unittest.TestCase):
         expected = "12"
         assert actual == expected
 
+    # def test_get_price_statistics(self):
+    #     trading_day_data = [
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "12:18:50",
+    #             "symbol": "AAPL",
+    #             "price": "142.64",
+    #         },
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "12:25:22",
+    #             "symbol": "AMD",
+    #             "price": "13.86",
+    #         },
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "14:25:25",
+    #             "symbol": "AAPL",
+    #             "price": "141.64",
+    #         },
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "14:25:28",
+    #             "symbol": "AMZN",
+    #             "price": "845.61",
+    #         },
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "16:28:50",
+    #             "symbol": "AAPL",
+    #             "price": "140.64",
+    #         },
+    #         {
+    #             "date": "2017-01-03",
+    #             "time": "16:29:59",
+    #             "symbol": "FB",
+    #             "price": "140.34",
+    #         },
+    #     ]
+    #     actual_price_statistics = feed_parser.get_price_statistics(trading_day_data)
+    #     expected = [
+    #         (
+    #             "AAPL",
+    #             {
+    #                 "date": "2017-01-03",
+    #                 "time": "16:28:50",
+    #                 "high": "142.64",
+    #                 "low": "140.64",
+    #             },
+    #         ),
+    #         (
+    #             "AMD",
+    #             {
+    #                 "date": "2017-01-03",
+    #                 "time": "12:25:22",
+    #                 "high": "13.86",
+    #                 "low": "13.86",
+    #             },
+    #         ),
+    #         (
+    #             "AMZN",
+    #             {
+    #                 "date": "2017-01-03",
+    #                 "time": "14:25:28",
+    #                 "high": "845.61",
+    #                 "low": "845.61",
+    #             },
+    #         ),
+    #         (
+    #             "FB",
+    #             {
+    #                 "date": "2017-01-03",
+    #                 "time": "16:29:59",
+    #                 "high": "140.34",
+    #                 "low": "140.34",
+    #             },
+    #         ),
+    #     ]
+    #     assert expected == actual_price_statistics
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
