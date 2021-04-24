02e5c7c5cfb00cd856d7fb0ee3df9c65fcd9a0aa
Mason Lin <pizza0117@gmail.com>
2021-04-24 13:32:13 +0800

    refactoring

--- feed_parser.py
+++ feed_parser.py
@@ -24,42 +24,31 @@ class ParsedFeedData:
     data_format_start_end: dict = None
 
 
-def get_most_active_hour(parsed_feed_data, trading_day):
+def get_most_active_hour(trading_day_data):
     cnt = Counter()
-    for i in range(len(parsed_feed_data.data_format_row)):
-        if parsed_feed_data.data_format_row[i]["date"] == trading_day:
-            # HH:MM:SS
-            hour = parsed_feed_data.data_format_row[i]["time"].split(":")[0]
-            cnt[hour] += 1
+    for data in trading_day_data:
+        hour = data["time"].split(":")[0]  # HH:MM:SS
+        cnt[hour] += 1
 
     # [0][0] means get hour from [('12', 3), ('16', 3)]
     sorted_most_common = sorted(cnt.most_common(), key=itemgetter(0))
     return sorted_most_common[0][0]
 
 
-def get_most_active_symbol(parsed_feed_data, trading_day):
+def get_most_active_symbol(trading_day_data):
     cnt = Counter()
-    for i in range(len(parsed_feed_data.data_format_row)):
-        if parsed_feed_data.data_format_row[i]["date"] == trading_day:
-            cnt[parsed_feed_data.data_format_row[i]["symbol"]] += 1
+    for data in trading_day_data:
+        cnt[data["symbol"]] += 1
     sorted_most_common = sorted(cnt.most_common(), key=itemgetter(0))
     return sorted_most_common[0][0]
 
 
-def get_last_quote_time(parsed_feed_data, trading_day):
-    for i in range(len(parsed_feed_data.data_format_row) - 1, 0, -1):
-        if parsed_feed_data.data_format_row[i]["date"] == trading_day:
-            last_quote_time = parsed_feed_data.data_format_row[i]["time"]
-            break
-    return last_quote_time
+def get_last_quote_time(trading_day_data):
+    return trading_day_data[-1]["time"]
 
 
-def get_valid_quote_count(parsed_feed_data, trading_day):
-    valid_quote_count = 0
-    for i in range(len(parsed_feed_data.data_format_row)):
-        if parsed_feed_data.data_format_row[i]["date"] == trading_day:
-            valid_quote_count += 1
-    return valid_quote_count
+def get_valid_quote_count(trading_day_data):
+    return len(trading_day_data)
 
 
 def quiz_a(parsed_feed_data):
@@ -77,12 +66,15 @@ def quiz_a(parsed_feed_data):
     alphabetically).
     """
     for trading_day in sorted(set(parsed_feed_data.data_format_col["date"])):
+        start = parsed_feed_data.data_format_start_end[trading_day]["start"]
+        end = parsed_feed_data.data_format_start_end[trading_day]["end"]
+        trading_day_data = parsed_feed_data.data_format_row[start:end]
         print(
             f"\n===Trading Day: {trading_day}===\n"
-            f"Last Quote Time: {get_last_quote_time(parsed_feed_data, trading_day)}\n"
-            f"Number of valid quotes: {get_valid_quote_count(parsed_feed_data, trading_day)}\n"
-            f"Most active hour: {get_most_active_hour(parsed_feed_data, trading_day)}\n"
-            f"Most active symbol: {get_most_active_symbol(parsed_feed_data, trading_day)}\n"
+            f"Last Quote Time: {get_last_quote_time(trading_day_data)}\n"
+            f"Number of valid quotes: {get_valid_quote_count(trading_day_data)}\n"
+            f"Most active hour: {get_most_active_hour(trading_day_data)}\n"
+            f"Most active symbol: {get_most_active_symbol(trading_day_data)}\n"
         )
 
 
@@ -144,13 +136,14 @@ def read_input(input):
     for i in range(len(data_format_row)):
         trading_day = data_format_row[i]["date"]
         if data_format_start_end.get(trading_day):
-            data_format_start_end[trading_day]["end"] = i
+            data_format_start_end[trading_day]["end"] = i + 1
         else:
-            data_format_start_end[trading_day] = {"start": i, "end": i}
+            data_format_start_end[trading_day] = {"start": i, "end": i + 1}
 
     logging.debug(pprint.pformat(data_format_start_end))
     logging.debug(pprint.pformat(data_format_row))
     logging.debug(pprint.pformat(data_format_col))
+    # maybe I can remove data_format_col, after finish quiz 2 and see.
     parsed_feed_data = ParsedFeedData(
         data_format_col, data_format_row, data_format_start_end
     )
